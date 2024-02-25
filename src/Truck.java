abe26e26d1123eb7d9a5ff8fe1b3ac61fcd484e9
latsa <latsapnev01@mail.ru>
2024-02-25 11:57:55 +0300

    Применил принцип наследования к классам: "Bicycle", "Car" и "Truck".

--- src/Truck.java
+++ src/Truck.java
@@ -1,15 +1,7 @@
-public class Truck {
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+public class Truck extends EngineVehicles {
+    public Truck(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
     }
-
-    public void checkEngine() {
-        System.out.println("Проверяем двигатель");
-    }
-
     public void checkTrailer() {
         System.out.println("Проверяем прицеп");
     }
