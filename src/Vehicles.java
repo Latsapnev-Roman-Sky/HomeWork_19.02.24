970d6a70dd003383168a5048d82101ba94ed1105
latsa <latsapnev01@mail.ru>
2024-02-25 12:10:26 +0300

    Исправил класс "Main", выполнил все задания.

--- src/Vehicles.java
+++ src/Vehicles.java
@@ -11,7 +11,7 @@ public class Vehicles implements Serviced {
     }
     @Override
     public void check() {
-        System.out.println("Обслуживаем"+ modelName);
+        System.out.println("Обслуживаем "+ modelName);
         for (int i = 0; i < wheelsCount; i++) {
             updateTyre();
         }
