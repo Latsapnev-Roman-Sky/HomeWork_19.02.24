5702cb09bc0ec648a1b827cd35598eb6ccb99a09
latsa <latsapnev01@mail.ru>
2024-02-25 11:54:34 +0300

    Создал класс родитель "EngineVehicles" для классов наследников "Car" и "Truck".

---
+++ src/EngineVehicles.java
@@ -0,0 +1,8 @@
+public class EngineVehicles extends Vehicles {
+    public EngineVehicles(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
